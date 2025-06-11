df49259f1895d75d56cd6a5df52d1e9cf03fd47f
Timur Kasimov <106226856+timakasimovktr@users.noreply.github.com>
2025-06-11 21:08:26 +0500

    update

--- src/app/payment/page.tsx
+++ src/app/payment/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import PaymentComponent from "@/components/PaymentComponent";
 
-const PaymentPage = () => {
+const Payment = () => {
   return (
     <>
       <PaymentComponent />
@@ -9,4 +9,4 @@ const PaymentPage = () => {
   )
 };
 
-export default PaymentPage;
+export default Payment;
